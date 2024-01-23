ec48e45aef82745cf600cc7414d3f8b81fe37f4b
dhruvd70 <dhruv.dohare04@gmail.com>
2024-01-23 22:00:55 +0530

    +block meta data struct and macros

--- mem_manager/mem_manager.h
+++ mem_manager/mem_manager.h
@@ -3,6 +3,11 @@
 #include <assert.h>
 
 #define MAX_STRUCT_NAME_SIZE        32
+
+typedef enum{
+    MM_FALSE,
+    MM_TRUE
+}vm_bool_e;
 typedef struct vm_page_family_{
     char struct_name[MAX_STRUCT_NAME_SIZE];
     uint32_t struct_size;
@@ -12,6 +17,29 @@ typedef struct vm_page_for_families_{
     struct vm_page_for_families_ *pNext;
     vm_page_family_t vm_page_family[0];
 }vm_page_for_families_t;
+
+typedef struct block_meta_data_{
+    vm_bool_e is_free;
+    uint32_t block_size;
+    uint32_t offset;
+    struct block_meta_data_ *p_prev_block;
+    struct block_meta_data_ *p_next_block;
+}block_meta_data_t;
+
+#define OFFSET_OF(container_struct, field_name)         \
+        ((size_t) & (((container_struct *) 0)->field_name))
+
+#define GET_PAGE_FROM_META_BLOCK(block_meta_data_ptr)   \
+        ((void *)((char *)block_meta_data_ptr - block_meta_data_ptr->offset))
+
+#define NEXT_META_BLOCK(block_meta_data_ptr)            \
+        (block_meta_data_ptr->p_next_block)
+
+#define PREV_META_BLOCK(block_meta_data_ptr)            \
+        (block_meta_data_ptr->p_prev_block)
+#define NEXT_META_BLOCK_BY_SIZE(block_meta_data_ptr)    \
+        (block_meta_data_t *) ((char *)(block_meta_data_ptr + 1)\
+        + block_meta_data_ptr->block_size)
 
 #define MAX_FAMILIES_PER_VM_PAGE    \
         (SYSTEM_PAGE_SIZE - sizeof(vm_page_for_families_t))/    \
